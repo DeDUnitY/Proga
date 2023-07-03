48ec6e337241f0b7abe85a47d9e4400f50d687fa
DeDUnitY <76110548+DeDUnitY@users.noreply.github.com>
2023-07-03 14:25:27 +0200

    Update main.cpp

--- 2 semestr/prac_4/main.cpp
+++ 2 semestr/prac_4/main.cpp
@@ -1,89 +1,82 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
-class Appliance
-{
+class Appliance {
 private:
     std::string i_name;
 public:
-    explicit Appliance(std::string& name) : i_name(name) {}
+    explicit Appliance(std::string &name) : i_name(name) {}
 
-    void Poll()
-    {
+    virtual void Poll() {
         std::cout << i_name << std::endl;
     }
 };
 
-class Electric_energy_meters : public Appliance
-{
+class Electric_energy_meters : public Appliance {
 public:
-    explicit Electric_energy_meters(std::string& name) : Appliance(name) {}
+    explicit Electric_energy_meters(std::string &name) : Appliance(name) {}
+
+    void Poll() override {
+        std::cout << "Electric energy meters" << std::endl;
+    }
 };
 
-class Input_descrete_signal : public Appliance
-{
+class Input_descrete_signal : public Appliance {
 public:
-    explicit Input_descrete_signal(std::string& name) : Appliance(name) {}
+    explicit Input_descrete_signal(std::string &name) : Appliance(name) {}
+
+    void Poll() override {
+        std::cout << "Input descrete signal" << std::endl;
+    }
 };
 
-class Heating_control : public Appliance
-{
+class Heating_control : public Appliance {
 public:
-    explicit Heating_control(std::string& name) : Appliance(name) {}
+    explicit Heating_control(std::string &name) : Appliance(name) {}
+
+    void Poll() override {
+        std::cout << "Heating control" << std::endl;
+    }
 };
 
-std::string electricity_meter[] = { "Mercury 230", "Neva MT314", "Energomera CE308" };
+std::string electricity_meter[] = {"Mercury 230", "Neva MT314", "Energomera CE308"};
+std::string input_descrete_signal[] = {"Reallab NL-16HV", "Priboreltctro PRE - 16", "Energoservis EHMB-1-24"};
+std::string heating_control[] = {"Ouman S203", "Oven TPM232"};
 
-std::string input_descrete_signal[] = { "Reallab NL-16HV", "Priboreltctro PRE - 16", "Energoservis EHMB-1-24" };
 
-std::string heating_control[] = { "Ouman S203", "Oven TPM232" };
-
-struct configuration
-{
-    Electric_energy_meters* meter = nullptr;
-    Input_descrete_signal* device = nullptr;
-    Heating_control* heat = nullptr;
-};
-
-int main()
-{
-    configuration config;
+int main() {
+    std::vector<Appliance *> config;
     std::ifstream file("../appliance.txt");
     std::string tmp;
-    while (getline(file, tmp))
-    {
+    while (getline(file, tmp)) {
         int n = sizeof(electricity_meter) / sizeof(std::string);
-        for (int i = 0; i < n; ++i)
-        {
-            if (electricity_meter[i] == tmp)
-            {
-                config.meter = new Electric_energy_meters(tmp);
+        for (int i = 0; i < n; ++i) {
+            if (electricity_meter[i] == tmp) {
+                config.push_back(new Electric_energy_meters(tmp));
                 break;
             }
         }
 
         n = sizeof(input_descrete_signal) / sizeof(std::string);
-        for (int i = 0; i < n; ++i)
-        {
-            if (input_descrete_signal[i] == tmp)
-            {
-                config.device = new Input_descrete_signal(tmp);
+        for (int i = 0; i < n; ++i) {
+            if (input_descrete_signal[i] == tmp) {
+                config.push_back(new Input_descrete_signal(tmp));
                 break;
             }
         }
 
         n = sizeof(heating_control) / sizeof(std::string);
-        for (int i = 0; i < n; ++i)
-        {
-            if (heating_control[i] == tmp)
-            {
-                config.heat = new Heating_control(tmp);
+        for (int i = 0; i < n; ++i) {
+            if (heating_control[i] == tmp) {
+                config.push_back(new Heating_control(tmp));
                 break;
             }
         }
     }
-    config.device->Poll();
-    config.heat->Poll();
-    config.meter->Poll();
+    for (Appliance *appli: config)
+        appli->Poll();
+    for (Appliance *conf: config)
+        delete conf;
 }
